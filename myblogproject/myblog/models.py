079e2c89a35b6fba2221e586fce82b018c8a2c9e
threegenie <63702924+threegenie@users.noreply.github.com>
2020-08-25 22:34:07 +0900

    add myblog files

---
+++ myblogproject/myblog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+# 이미 배운 내용의 설명은 생략합니다.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('myblog.Post', on_delete = models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
